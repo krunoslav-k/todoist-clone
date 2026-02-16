a1fa92f2cb0a553cdeef111cf9fd9b4f3cf513e2
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-02-16 19:28:23 +0100

    feat: toggle todo completed on todo checkbox click

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from "react";
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/dummyData";
 
 function App() {
+  const [todos, setTodos] = useState(dummyData);
+
+  function toggleCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo)),
+    );
+  }
+
   return (
     <>
       <div>
-        {dummyData.map((todo) => {
-          return <TodoItem todo={todo} />;
+        {todos.map((todo) => {
+          return (
+            <TodoItem
+              todo={todo}
+              onToggleCompleted={toggleCompleted}
+              key={todo.id}
+            />
+          );
         })}
       </div>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -2,13 +2,18 @@ import type Todo from "../types/todo";
 
 interface TodoItemProps {
   todo: Todo;
+  onToggleCompleted: (id: number, completed: boolean) => void;
 }
 
-export default function TodoItem({ todo }: TodoItemProps) {
+export default function TodoItem({ todo, onToggleCompleted }: TodoItemProps) {
   return (
     <div>
       <label>
-        <input type="checkbox" checked={todo.completed} />
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={(e) => onToggleCompleted(todo.id, e.target.checked)}
+        />
         <span>{todo.title}</span>
         <span>{todo.description}</span>
       </label>
