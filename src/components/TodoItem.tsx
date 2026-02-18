ebf467458cc2da7acbd3ba13cadd31b6b8dc0738
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-02-18 08:52:32 +0100

    style: style TodoItem layout and custom checkbox

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -7,15 +7,37 @@ interface TodoItemProps {
 
 export default function TodoItem({ todo, onToggleCompleted }: TodoItemProps) {
   return (
-    <div>
-      <label>
+    <div className="p-3 border-b-1 border-gray-300">
+      <label className="flex items-center justify-start">
         <input
           type="checkbox"
           checked={todo.completed}
           onChange={(e) => onToggleCompleted(todo.id, e.target.checked)}
+          className="hidden"
         />
-        <span>{todo.title}</span>
-        <span>{todo.description}</span>
+
+        <span className="w-5 h-5 rounded-full border border-gray-500 flex items-center justify-center mr-3">
+          <svg
+            className={`w-3.5 h-3.5 text-gray-500 transition-all duration-150  ${todo.completed ? "opacity-100 scale-100" : "opacity-0 scale-75 hover:opacity-50 hover:scale-100"}`}
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2.125"
+            viewBox="0 0 24 24"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+        </span>
+
+        <div>
+          <span className="ml-2">{todo.title}</span>
+          <div className="ml-2 text-[0.8rem] text-gray-400">
+            {todo.description}
+          </div>
+        </div>
       </label>
     </div>
   );
