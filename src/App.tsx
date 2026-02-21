624603c4f9cefcfbde0d6b197db76b642d1f7fb9
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-02-21 10:35:45 +0100

    style: style button for showing AddTodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/dummyData";
 import AddTodoForm from "./components/AddTodoForm";
 import type Todo from "./types/todo";
+import { Plus } from "lucide-react";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -39,8 +40,20 @@ function App() {
         })}
 
         {!isAddTodoFormOpen && (
-          <button onClick={() => setIsAddTodoFormOpen((prev) => !prev)}>
-            Add task
+          <button
+            onClick={() => setIsAddTodoFormOpen((prev) => !prev)}
+            className="flex items-start justify-center p-3 group"
+          >
+            <span className="w-5 h-5 mr-4 rounded-full border border-transparent flex items-center justify-center group-hover:border-gray-700 group-hover:bg-gray-700">
+              <Plus
+                size={28}
+                strokeWidth={1}
+                className="text-gray-700 group-hover:text-white"
+              />
+            </span>
+            <span className="text-[0.9rem] text-gray-500 group-hover:text-gray-800">
+              Add task
+            </span>
           </button>
         )}
       </div>
