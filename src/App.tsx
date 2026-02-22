35f9b2a862fe944b47f34075f67b466f2cc1218f
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-02-22 10:50:56 +0100

    refactor: extract AddTodoButton component and rename handlers

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/dummyData";
 import AddTodoForm from "./components/AddTodoForm";
 import type Todo from "./types/todo";
-import { Plus } from "lucide-react";
+import AddTodoButton from "./components/AddTodoButton";
 
 function App() {
   const [todos, setTodos] = useState(dummyData);
@@ -25,7 +25,11 @@ function App() {
     setTodos((prevTodos) => [...prevTodos, newTodo]);
   }
 
-  function cancelAddTodo() {
+  function showAddTodoForm() {
+    setIsAddTodoFormOpen((prev) => !prev);
+  }
+
+  function hideAddTodoForm() {
     setIsAddTodoFormOpen(false);
   }
 
@@ -44,27 +48,13 @@ function App() {
         })}
 
         {!isAddTodoFormOpen && (
-          <button
-            onClick={() => setIsAddTodoFormOpen((prev) => !prev)}
-            className="flex items-start justify-center p-3 group"
-          >
-            <span className="w-5 h-5 mr-4 rounded-full border border-transparent flex items-center justify-center group-hover:border-gray-700 group-hover:bg-gray-700">
-              <Plus
-                size={28}
-                strokeWidth={1}
-                className="text-gray-700 group-hover:text-white"
-              />
-            </span>
-            <span className="text-[0.9rem] text-gray-500 group-hover:text-gray-800">
-              Add task
-            </span>
-          </button>
+          <AddTodoButton handleAddTodoButtonClick={showAddTodoForm} />
         )}
 
         {isAddTodoFormOpen && (
           <AddTodoForm
             handleAddTodo={addTodo}
-            handleCancelAddTodo={cancelAddTodo}
+            handleCancelAddTodo={hideAddTodoForm}
           />
         )}
       </div>
---
+++ src/components/AddTodoButton.tsx
@@ -0,0 +1,27 @@
+import { Plus } from "lucide-react";
+
+interface AddTodoButtonProps {
+  handleAddTodoButtonClick: () => void;
+}
+
+export default function AddTodoButton({
+  handleAddTodoButtonClick,
+}: AddTodoButtonProps) {
+  return (
+    <button
+      onClick={handleAddTodoButtonClick}
+      className="flex items-start justify-center p-3 group"
+    >
+      <span className="w-5 h-5 mr-4 rounded-full border border-transparent flex items-center justify-center group-hover:border-gray-700 group-hover:bg-gray-700">
+        <Plus
+          size={28}
+          strokeWidth={1}
+          className="text-gray-700 group-hover:text-white"
+        />
+      </span>
+      <span className="text-[0.9rem] text-gray-500 group-hover:text-gray-800">
+        Add task
+      </span>
+    </button>
+  );
+}
