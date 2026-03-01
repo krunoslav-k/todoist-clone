822aff06a9d89c9eff4541cba6c15bcafc73bf6c
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-03-01 13:44:59 +0100

    feat: add quick date buttons to DatePicker

--- src/components/AddTodoForm/dropdowns/DatePicker.tsx
+++ src/components/AddTodoForm/dropdowns/DatePicker.tsx
@@ -1,6 +1,14 @@
-import { isValid, parse } from "date-fns";
+import { addDays, isValid, nextSaturday, parse } from "date-fns";
 import { format } from "date-fns/format";
-import { CalendarPlus, ChevronLeft, ChevronRight, X } from "lucide-react";
+import {
+  Calendar1,
+  CalendarPlus,
+  ChevronLeft,
+  ChevronRight,
+  Sofa,
+  Sun,
+  X,
+} from "lucide-react";
 import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import { enGB } from "react-day-picker/locale/en-GB";
@@ -81,7 +89,7 @@ export default function DatePicker({ handleSelectDate }: DatePickerProps) {
       {isDateCompleteAndValid && (
         <button
           onClick={() => handleSelectDate(parsedDate)}
-          className="py-2 -mx-3 flex justify-start items-center border-t  border-b border-gray-200 hover:cursor-pointer"
+          className="py-2 -mx-3 flex justify-start items-center border-t border-gray-200 hover:cursor-pointer"
         >
           <CalendarPlus
             strokeWidth={1}
@@ -94,6 +102,62 @@ export default function DatePicker({ handleSelectDate }: DatePickerProps) {
           </div>
         </button>
       )}
+
+      <div className="-mx-3 py-1 border-t border-b border-gray-200">
+        <div className="h-full flex justify-between items-stretch group">
+          <button
+            onClick={() => handleSelectDate(new Date())}
+            className="w-full py-2 flex justify-start items-center text-sm group-hover:bg-gray-100 hover:cursor-pointer"
+          >
+            <Calendar1
+              size={20}
+              strokeWidth={1.25}
+              color="#4b9344"
+              className="mx-3"
+            />
+            Today
+          </button>
+          <div className="flex items-center pr-4 text-xs font-light text-gray-500 group-hover:bg-gray-100">
+            {format(new Date(), "eee")}
+          </div>
+        </div>
+
+        <div className="h-full flex justify-between items-stretch group">
+          <button
+            onClick={() => handleSelectDate(addDays(new Date(), 1))}
+            className="w-full py-2 flex justify-start items-center text-sm group-hover:bg-gray-100 hover:cursor-pointer"
+          >
+            <Sun
+              size={20}
+              strokeWidth={1.25}
+              color="#ad6200"
+              className="mx-3"
+            />
+            Tomorrow
+          </button>
+          <div className="flex items-center pr-4 text-xs font-light text-gray-500 group-hover:bg-gray-100">
+            {format(addDays(new Date(), 1), "eee")}
+          </div>
+        </div>
+
+        <div className="h-full flex justify-between items-stretch group">
+          <button
+            onClick={() => handleSelectDate(nextSaturday(new Date()))}
+            className="w-full py-2 flex justify-start items-center text-sm group-hover:bg-gray-100 hover:cursor-pointer"
+          >
+            <Sofa
+              size={20}
+              strokeWidth={1.25}
+              color="#2570e0"
+              className="mx-3"
+            />
+            Next weekend
+          </button>
+          <div className="w-fit whitespace-nowrap flex items-center pr-4 text-xs font-light text-gray-500 group-hover:bg-gray-100">
+            {format(nextSaturday(new Date()), "eee d LLL")}
+          </div>
+        </div>
+      </div>
 
       <DayPicker
         animate
