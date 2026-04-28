f7d57a1ea4b6e6bb8f790e5341d0a0b0eda4281d
Krunoslav Kazalicki <krunoslavk@hotmail.com>
2026-04-28 18:06:50 +0200

    style: update nav icons active state and refine sidebar heading

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,7 +5,12 @@ import SidebarProjects from "./SidebarProjects";
 export default function Sidebar() {
   return (
     <div className="h-full shrink-0 flex flex-col gap-2 w-xs p-4 bg-gray-50 ">
-      <h2>SIDEBAR</h2>
+      <h2 className="p-3 mb-1 text-xl">
+        <span className="font-bold bg-linear-to-br from-[#ff5f54] via-[#e44332] to-[#db3727] bg-clip-text text-transparent">
+          Todoist
+        </span>{" "}
+        clone
+      </h2>
 
       <nav className="flex flex-col ">
         <SidebarActions />
--- src/components/Sidebar/SidebarNavigation.tsx
+++ src/components/Sidebar/SidebarNavigation.tsx
@@ -9,31 +9,11 @@ import { NavLink } from "react-router-dom";
 
 export default function SidebarNavigation() {
   const navButtons = [
-    {
-      Icon: Inbox,
-      label: "Inbox",
-      path: "/inbox",
-    },
-    {
-      Icon: Calendar1,
-      label: "Today",
-      path: "/today",
-    },
-    {
-      Icon: CalendarDays,
-      label: "Upcoming",
-      path: "/upcoming",
-    },
-    {
-      Icon: Component,
-      label: "Filters & Labels",
-      path: "/filters&labels",
-    },
-    {
-      Icon: CircleCheckBig,
-      label: "Completed",
-      path: "/completed",
-    },
+    { Icon: Inbox, label: "Inbox", path: "/inbox" },
+    { Icon: Calendar1, label: "Today", path: "/today" },
+    { Icon: CalendarDays, label: "Upcoming", path: "/upcoming" },
+    { Icon: Component, label: "Filters & Labels", path: "/filters&labels" },
+    { Icon: CircleCheckBig, label: "Completed", path: "/completed" },
   ];
 
   return (
@@ -44,10 +24,36 @@ export default function SidebarNavigation() {
             key={path}
             to={path}
             className={({ isActive }) =>
-              `sidebar_button ${isActive ? "bg-gray-200" : ""}`
+              `sidebar_button flex items-center gap-3 ${isActive ? "bg-gray-200" : ""}`
             }
           >
-            <Icon size={20} strokeWidth={1.3} /> {label}
+            {({ isActive }) => (
+              <>
+                <div className="relative w-5 h-5 shrink-0">
+                  {isActive && (
+                    <Icon
+                      size={20}
+                      strokeWidth={1.3}
+                      fill="#39485e"
+                      className="absolute inset-0 text-[#39485e]"
+                    />
+                  )}
+                  <Icon
+                    size={20}
+                    strokeWidth={1.3}
+                    className={`absolute inset-0 z-10  ${
+                      isActive ? "text-white" : "text-gray-600"
+                    }`}
+                  />
+                </div>
+
+                <span
+                  className={`truncate ${isActive ? "font-medium text-gray-900" : "text-gray-700"}`}
+                >
+                  {label}
+                </span>
+              </>
+            )}
           </NavLink>
         );
       })}
